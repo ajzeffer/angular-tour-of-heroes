d1dfeea5b37d82ff284d1de82308aec984fb81f8
Adam Zeffer <az@prodiginet.com>
2018-05-11 12:34:48 -0700

    init

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Mr. Bad'},
+  {id: 12, name: 'Mr. Zeffer'},
+  {id: 14, name: 'Mr. Slow'},
+  {id: 13, name: 'Mrs. Fast'},
+  {id: 15, name: 'Mrs. Turrible'},
+  {id: 17, name: 'Mrs. Pretty'},
+  {id: 19, name: 'Mrs. Bad'},
+  {id: 32, name: 'Mr. Nice'},
+  {id: 45, name: 'Mr. Good'}
+];
